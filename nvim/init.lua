cc9267a7bf125efa79946c3057e4d759d768a811
dnakhooda <dnakhooda@gmail.com>
2025-08-24 21:54:45 -0400

    nvim config restructure

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,11 +2,11 @@
 require("config.globals")
 require("config.options")
 require("config.keymaps")
-require("config.lsps")
-require("config.colorscheme")
 
 -- Plugin Configs
-require("plugins.treesitterConfig")
-require("plugins.telescopeConfig")
-require("plugins.undotreeConfig")
-require("plugins.fugitiveConfig")
+require("config.lsps")
+require("config.colorscheme")
+require("config.treesitterConfig")
+require("config.telescopeConfig")
+require("config.undotreeConfig")
+require("config.fugitiveConfig")
